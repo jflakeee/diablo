d5a0677ed9c7529ea61582f19357b55dd2464ae5
jflakeee <jflake@naver.com>
2026-09-23 12:50:18 +0900

    Deploy quest campaign build 2f33ad4

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790128085|12480608';
+const CACHE_VERSION = '1790135374|8700438';
 /** @type {string} */
 const CACHE_PREFIX = 'Ashen Depths-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
