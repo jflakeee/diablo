6f2ebcbb74aa3d08a006ad0a43394a7e1a0570d3
jflakeee <jflake@naver.com>
2026-09-23 15:00:10 +0900

    Deploy waypoint build b6a9e9d

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790141145|8676413';
+const CACHE_VERSION = '1790143177|7969371';
 /** @type {string} */
 const CACHE_PREFIX = 'Ashen Depths-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
