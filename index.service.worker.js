569f4318c5945a1a9756df3d74fb5b8626c08947
jflakeee <jflake@naver.com>
2026-09-24 20:46:28 +0900

    Deploy mercenary equipment build 5d4dff2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790171425|7497106';
+const CACHE_VERSION = '1790250363|11061330';
 /** @type {string} */
 const CACHE_PREFIX = 'Ashen Depths-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
