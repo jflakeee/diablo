89400ae1b5a7f1f151faae13f9c952baab2f8dfe
jflakeee <jflake@naver.com>
2026-09-24 22:33:59 +0900

    Deploy stamina build 7487c39

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790250363|11061330';
+const CACHE_VERSION = '1790256812|7960924';
 /** @type {string} */
 const CACHE_PREFIX = 'Ashen Depths-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
