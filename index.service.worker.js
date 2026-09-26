e15f28967a6eb81a58291b1321d3f0d5f7969202
jflakeee <jflake@naver.com>
2026-09-26 11:53:26 +0900

    Deploy compact mobile UI build 1e55543

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790390385|8101205';
+const CACHE_VERSION = '1790391156|7815509';
 /** @type {string} */
 const CACHE_PREFIX = 'Ashen Depths-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
