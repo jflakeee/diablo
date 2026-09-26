6acf4698652a9368d171960326e297d0939a9516
jflakeee <jflake@naver.com>
2026-09-26 13:21:15 +0900

    Deploy glyph cleanup c175ac9

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790396185|7796460';
+const CACHE_VERSION = '1790396454|10027587';
 /** @type {string} */
 const CACHE_PREFIX = 'Ashen Depths-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
