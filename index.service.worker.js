d5f97e9606f9d7bb7fff887f1257da5f52f9b0ec
jflakeee <jflake@naver.com>
2026-09-26 12:18:03 +0900

    Deploy potion glyph fix 411e0f5

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790391156|7815509';
+const CACHE_VERSION = '1790392664|7700161';
 /** @type {string} */
 const CACHE_PREFIX = 'Ashen Depths-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
